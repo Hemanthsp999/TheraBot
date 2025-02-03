60ac4e455de7905e83e0d925ada3cdcef19867e5
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-02-04 03:10:40 +0530

    -Add Navbar, footer and images to the UI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './components/css/App.css';
 import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 
 function App() {
     return (
         <>
             <div className="App">
                 <Navbar/>
+            </div>
+            <div className="App">
+                <Footer/>
             </div>
         </>
     );
