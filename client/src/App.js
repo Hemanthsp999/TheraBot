b4e13ae16f768e492cd519a931f8e35eb6255527
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-02-04 22:03:27 +0530

    -Add Nav and Footer files

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
+import { Outlet, useNavigate } from 'react-router-dom';
 import './components/css/App.css';
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer.js';
 
 function App() {
+    const navigate = useNavigate();
     return (
-        <>
-            <div className="App">
-                <Navbar/>
+        <div className="app-container">
+            <Navbar />
+              {/* Hero Section */}
+            <center>
+            <section className=" mt-3 hero text-lg">
+                <h1> Meet TheraBot: Your AI Mental Health Companion</h1>
+                <p> Your safe space to express, reflect and heal - anytime, anywhere.</p>
+            </section>
+            </center>
+            
+            
+            <div className="content">
+                <Outlet />
             </div>
-            <div className="App">
-                <Footer/>
-            </div>
-        </>
+            <Footer />
+        </div>
     );
 }
 
 export default App;
+
