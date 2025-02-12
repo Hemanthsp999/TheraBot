67c8083cb67211ef7064eee4955e0c9f40f10b0b
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-02-12 23:54:46 +0530

    -Add server handler.

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class Phone_Number(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True)
 
 
 class User(models.Model):
@@ -12,5 +7,5 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=10, unique=True, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return self.username
--- backend/server/api/views.py
+++ backend/server/api/views.py
@@ -1,24 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-# from django.contrib.auth.models import User
 from api.models import User
 from django.contrib.auth.hashers import make_password, check_password
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+from datetime import timedelta
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model
+        model = User
         fields = ['username', 'phone_number', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        validated_data['password'] = make_password(validated_data['password'])
+        return User.objects.create(**validated_data)
 
 
 class RegisterView(APIView):
@@ -37,16 +37,43 @@ class LoginView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, format=None):
-        username = request.data.get('username')
+        email = request.data.get('email')
         password = request.data.get('password')
 
-        if not username or not password:
+        if not email or not password:
             return Response({'error': 'Both username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = authenticate(username=username, password=password)
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        print(f'Login: {email, check_password(password, user.password)}')
 
-        if user is None:
+        if not check_password(password, user.password):
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        refresh = RefreshToken.for_user(user)
+        access_token_expiry = now() + timedelta(minutes=30)
+
+        return Response({'refresh': str(refresh), 'access_token': str(refresh.access_token),
+                         'redirect_url': '/', 'expires_at': access_token_expiry}, status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        try:
+            refresh_token = request.data.get('refresh_token')
+            if not refresh_token:
+                return Response({'error:' 'Refresh token is required'}, status=400)
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Logged out successfully"}, status=200)
+
+        except Exception as e:
+            return Response({"error": "Invalid token"}, status=400)
 
