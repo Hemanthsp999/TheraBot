1bbf2ea882d6c81e03bf130569e1f8ed1fbc9b7b
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-02-25 21:32:44 +0530

    - Add License for the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,20 @@
+/* 
+Copyright 2025 [Hemanth S P]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./components/css/index.css";
@@ -34,7 +51,7 @@ const router = createBrowserRouter([
   },
   // Separate route for ChatBot without App wrapper
   {
-        // Add Protected Route
+    // Add Protected Route
     path: "/chatbot",
     element: <Protected />,
     children: [{ index: true, element: <ChatBot /> }],
